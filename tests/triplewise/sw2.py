0ae128ddee2a347034c69a342b0eaf0f7f647d41
noabauma <noabauma@student.ethz.ch>
2021-03-10 11:17:57 +0100

    added sw2.py test case

---
+++ tests/triplewise/sw2.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+
+"""Test triplewise forces."""
+
+import mirheo as mir
+import numpy
+from autograd import grad
+import autograd.numpy as np
+import sys
+import h5py
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+
+#parameters
+rc = 2.0
+epsilon = 1.234
+A       = 1.234     #normally 1.910633236
+B       = 1.234
+sigma   = 1.234
+
+#Stillinger-Weber 3Body Potential
+def sw2_pot(r_i, r_j):
+    r_ij = r_i - r_j
+   
+    r = np.sqrt(np.dot(r_ij,r_ij))
+
+    return A*epsilon*(B*((sigma/r)**4) - 1.0)*np.exp(sigma/(r-rc))
+
+
+def withinCutOff(r,s):
+    rs = r - s
+    drs2 = np.dot(rs,rs)
+    if drs2 > rc*rc:
+        return False
+    else:
+        return True
+
+
+def brute_force(particle):
+    n = particle.shape[0]
+    forces = np.zeros((n,3))
+    grad_sw2_pot = grad(sw2_pot)
+
+    #2body
+    for i in range(n):
+        for j in range(n):
+            if i == j:
+                continue
+            if withinCutOff(particle[i], particle[j]):
+                forces[i,:] += -grad_sw2_pot(particle[i], particle[j])
+    return forces
+
+
+def main():
+    n = 32
+
+    #shape of particles = 2.0*np.random.rand(n, 3) + np.full((n, 3), 2.0)
+    #if rank == 0: numpy.savetxt('particles-sw3.csv', particles, fmt='%f', delimiter=',')
+    particles = np.loadtxt("particles-sw3.csv", delimiter=',')
+    vel = np.zeros((n,3))
+    
+    ranks = (2, 2, 2)   #force Halo intractions
+    domain = (8.0, 8.0, 8.0)
+
+    u = mir.Mirheo(ranks, domain, debug_level=3, log_filename='log', no_splash=True)
+
+    pv = mir.ParticleVectors.ParticleVector('pv', mass=1.0)
+    ic = mir.InitialConditions.FromArray(pos=particles, vel=vel)
+    u.registerParticleVector(pv, ic)
+
+    sw2 = mir.Interactions.Pairwise('sw', rc, kind="SW", epsilon=epsilon, sigma=sigma, A=A, B=B)
+    u.registerInteraction(sw2)
+    u.setInteraction(sw2, pv, pv)
+
+    vv = mir.Integrators.VelocityVerlet('vv')
+    u.registerIntegrator(vv)
+    u.setIntegrator(vv, pv)
+
+
+    dump_every = 1
+    u.registerPlugins(mir.Plugins.createForceSaver('forces', pv))
+    u.registerPlugins(mir.Plugins.createDumpParticles('force_dump', pv, dump_every, ["forces"], 'h5/sw2-'))
+
+    u.run(2, dt=0.0001)
+
+
+    if rank == 0:
+        f = h5py.File('h5/sw2-00001.h5', 'r')
+        mirheo = f['forces'][()]
+        brute = brute_force(particles)
+        try:    
+            #if necessary sort them
+            numpy.testing.assert_allclose(np.sort(mirheo, axis=0), np.sort(brute, axis=0), rtol=1e-10)
+        except:
+            print("particles:\n", particles)
+            print("mirheo positions:\n", f['position'][()])
+            print("mirheo force:\n", mirheo)
+            print("brute force:\n", brute)
+            raise
+
+        print("OK")
+
+
+main()
+
+# TEST: triplewise.sw2
+# cd triplewise
+# mir.run --runargs "-n 16" ./sw2.py > sw2.out.txt
